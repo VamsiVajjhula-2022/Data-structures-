e1a762ccf7232cbc77ae071c5dfe678f9a9d9e89
MANDALEEKA AVSS VIGHNESH <109013315+mavssvighnesh@users.noreply.github.com>
2023-11-04 15:13:32 +0530

    added the comments to increase readability

--- Insertion_beginning.c
+++ Insertion_beginning.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 //Here we give data and next because they are the elements present in the linked list.
-struct Node
+struct Node   //defining the structure to define the nodes 
 {
-    int data;
-    struct Node *next;
+    int data;    //data variable of the node 
+    struct Node *next;  //pointer to parse through the nodes 
 };
 
-void Traverse(struct Node *ptr)
+void Traverse(struct Node *ptr)  //method to parse through nodes 
 {
     while (ptr!=NULL)
     {
@@ -17,7 +17,7 @@ void Traverse(struct Node *ptr)
     
 }
 
-struct Node *InsertBeg(struct Node *ptr,int data)
+struct Node *InsertBeg(struct Node *ptr,int data)   //method to insert nodes in begining 
 {
     struct Node *p;
     p=(struct Node *)malloc(sizeof(struct Node*));
@@ -25,28 +25,35 @@ struct Node *InsertBeg(struct Node *ptr,int data)
     p->next=ptr;
     return p;
 }
-int main()
+int main()   //main method 
 {
-    struct Node *head;
+    struct Node *head;  
     struct Node *second;
     struct Node *third;
 
+    //memory allocation for head second and third 
     head=(struct Node *)malloc(sizeof(struct Node*));
     second=(struct Node*)malloc(sizeof(struct Node*));
     third=(struct Node*)malloc(sizeof(struct Node*));
 
+    //adding the data and next values to the head 
     head->data=10;
     head->next=second;
 
+    //adding the data and next values to the second 
     second->data=20;
     second->next=third;
 
+    //adding the data and next values to the third
     third->data=30;
     third->next=NULL;
 
-   
+   //calling the traverse method with head as parameter
     Traverse(head);
+    //inserting a new node at begning 
     head=InsertBeg(head,50);
+
+    //prnting the list after insertion at begining 
     printf("After insertion of element in the beginning:\n");
     Traverse(head);
     return(0);
